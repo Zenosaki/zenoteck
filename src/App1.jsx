9095df18738ad91d10993bb6bbdf9efb480957be
Zenosaki <arselin.chaabanie@gmail.com>
2024-05-04 14:06:20 +0200

    i added the style and the silouette of the second page

--- src/App1.jsx
+++ src/App1.jsx
@@ -1,7 +1,9 @@
-
+import Header from './P2_components/header'
 
 export default function App1(){
   return(
-    <></>
+    <div className="main2_container">
+      <Header/>
+    </div>
   )
 }
---
+++ src/P2_components/header.jsx
@@ -0,0 +1,29 @@
+import Logo from '../assets/imgs/logo-icon.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-regular-svg-icons';
+import {faDumbbell} from '@fortawesome/free-solid-svg-icons';
+import {faKey} from '@fortawesome/free-solid-svg-icons';
+import {faCamera} from '@fortawesome/free-solid-svg-icons';
+import IntroBox from './components/introBox';
+
+export default function header(){
+  return(
+    <div className="Page2_Container">
+      <div className="Header2_Container">
+       <div className="left-content">
+         <img src={Logo} alt="logo" />
+         <h1><a href="index.html">Zeno<span>Teck</span></a></h1>
+       </div>
+       <div className="right_content">
+       <ol>
+          <li><a href="#Section3"><FontAwesomeIcon className='icon' icon={faKey} />Tutoriels</a></li>
+          <li><a href="#Section3"><FontAwesomeIcon className='icon' icon={faDumbbell} />Exercices</a></li>
+          <li><a href="#AboutMe"><FontAwesomeIcon className='icon' icon={faCamera} />Lives</a></li>
+          <li className='user-icon'><a href="#" className='user-line'><FontAwesomeIcon className='User'  icon={faUser}/></a></li>
+          </ol>
+       </div>
+      </div>
+      <IntroBox/>
+    </div>
+  )
+}
